bc6b90b32aa6d2d09c039cd9ee3cf1fe71974476
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-09 12:18:02 -0300

    adição do js em produtos

---
+++ exercicios/js/controle.js
@@ -0,0 +1,20 @@
+console.log("testando Js")
+
+var livros = [
+    ["Mick","mick.jpg"],
+    ["Quem somos nós", "quem-somos-nos.jpg"],
+    ["A cura quântica", "cura-quantica.jpg"],
+    ["O pequeno Príncipe", "pequeno-principe.jpg"]
+
+]
+console.log(livros)
+
+var galeria = document.querySelector("#galeria")
+
+for (var i= 0 ; i < livros.length; i++){
+    var img = new Image()
+    img.src="images/" + livros[i][1]
+    img.width=120
+    galeria.innerHTML+= "<p>" + livros[i][0] + "<br />"
+    galeria.appendChild(img)
+}
