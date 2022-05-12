68b9b110de6ad073c4b9e7c0acb7faf73d4e1fd5
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-12 09:53:55 -0300

    m

--- exercicios/js/exemplo-json.js
+++ exercicios/js/exemplo-json.js
@@ -1,12 +1,12 @@
-console.log("testando Js")
+// console.log("testando Js")
 
 // Escopos var let const
 let videos = '{"tipo": "Site de Assinatura", "turma":"Impulso Tec","ano":2022}'
 
-console.log(videos)
+// console.log(videos)
 let videosJSON = JSON.parse(videos)
 
-console.log(videosJSON)
+// console.log(videosJSON)
 
 let conteudo = document.querySelector("#conteudo")
 let h3 = document.querySelector("#conteudo h3")
@@ -15,5 +15,29 @@ let p2 = document.querySelector("#p2")
 
 
 h3.innerHTML = videosJSON.tipo
-p1.innerHTML ="Turma: " + videosJSON.turma
+p1.innerHTML = "Turma: " + videosJSON.turma
 p2.innerHTML = videosJSON.ano
+
+
+//JSON EXEMPLO 2
+
+let usuario = '{"nome":"Adalberto","idade":34,"livros":["Bill Gates","Terror em Lake City","O Elefante Branco","Capitão De Areia"]}'
+
+let usuarioJSON = JSON.parse(usuario)
+
+console.log(usuarioJSON)
+
+let infoNome = document.querySelector("#infoNome")
+infoNome.innerHTML = usuarioJSON.nome + ", " + usuarioJSON.idade + "anos"
+
+let listaLivros = document.querySelector('#listaLivros')
+listaLivros.innerHTML = '<li>' + usuarioJSON.livros[1] + '</li>'
+listaLivros.innerHTML += '<li>' + usuarioJSON.livros[2] + '</li>'
+
+
+let livrosFor = document.querySelector('#livrosFor')
+
+for (var i = 0; i < usuarioJSON.livros.length; i++) {
+    console.log(usuarioJSON.livros[i])
+    livrosFor.innerHTML += (i+1) + ': ' + usuarioJSON.livros[i] + '<br>'
+}
