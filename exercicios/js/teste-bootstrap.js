b855c2c8fd1e82a48dc1a6f5ee46ad8af9ad594f
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-16 09:38:06 -0300

    att

---
+++ exercicios/js/teste-bootstrap.js
@@ -0,0 +1,12 @@
+localStorage.aula = "javascript"
+
+localStorage.setItem("Projeto", "ImpulsoTec")
+
+///
+
+console.log(localStorage.Projeto)
+console.log(localStorage.getItem("Projeto"))
+
+// localStorage.removeItem("aula")
+
+localStorage.aula = 'Banco De Dados'
